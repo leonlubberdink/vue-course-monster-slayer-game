e7838063211827189d16ad7b708dfbed94223ccc
leonlubberdink <mail@leonlubberdink.com>
2024-05-15 21:56:25 +0200

    implemented healing

--- app.js
+++ app.js
@@ -3,30 +3,88 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
   computed: {
     monsterHealthBarStyles() {
       return this.getHealthBarStyles(this.monsterHealth);
     },
+
     playerHealthBarStyles() {
       return this.getHealthBarStyles(this.playerHealth);
+    },
+
+    specialAttackIsActive() {
+      return this.currentRound % 3 !== 0;
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        alert("Draw!");
+      } else if (value <= 0) {
+        alert("You lose!");
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        alert("Draw!");
+      } else if (value <= 0) {
+        alert("You win!");
+      }
     },
   },
   methods: {
     attackMonster() {
-      this.monsterHealth -= this.getRandomValue(5, 12);
+      this.currentRound++;
+      const attackValue = this.getRandomValue(5, 12);
+      if (this.monsterHealth - attackValue < 0) {
+        this.monsterHealth = 0;
+      } else {
+        this.monsterHealth -= attackValue;
+      }
       this.attackPlayer();
     },
+
     attackPlayer() {
-      this.playerHealth -= this.getRandomValue(8, 15);
+      const attackValue = this.getRandomValue(8, 15);
+      if (this.playerHealth - attackValue < 0) {
+        this.playerHealth = 0;
+      } else {
+        this.playerHealth -= attackValue;
+      }
     },
+
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = this.getRandomValue(9, 23);
+      if (this.monsterHealth - attackValue < 0) {
+        this.monsterHealth = 0;
+      } else {
+        this.monsterHealth -= attackValue;
+      }
+      this.attackPlayer();
+    },
+
+    healPlayer() {
+      this.currentRound++;
+      const healValue = this.getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
+
     getHealthBarStyles(health) {
       return {
         width: health + "%",
         backgroundColor: health > 50 ? "green" : health < 20 ? "red" : "orange",
       };
     },
+
     getRandomValue(min, max) {
       return Math.floor(Math.random() * (max - min) + min);
     },
